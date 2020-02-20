579ca3c2d3932416c66f83e3e296422c120e9844
Seb1903 <sebastien.martinez99@gmail.com>
2020-02-20 10:30:47 +0100

    First commit

---
+++ Exo2.py
@@ -0,0 +1,11 @@
+# Question 2 explications
+# En tapant python file.py dans le terminal, on peut lancer le fichier (et ses fonctions)
+# importer sys 
+# print('hello', sysargv[1]), sysargv représente les arguments de ce que l'on lance dans le terminal(file.py par ex)
+
+import sys 
+#print('hello', sysargv[1])
+with open(str(sysargv[1]), 'r') as file : 
+    texte = file.read()
+    print(texte)
+print(sysargv[1])
---
+++ exo1.py
@@ -0,0 +1,18 @@
+import re
+tel = input("numéro de téléphone : ")
+canvas =  r'\+[0-9]{2}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}'
+c = re.compile(canvas)
+print(c.match(tel) is not None)
+
+
+
+entier = r'\+?[1-9][0-9]*'
+entieri = input('votre entier : ')
+e = re.compile(entier)
+print(e.match(entieri) +is not None)
+
+#add git
+
+
+
+
