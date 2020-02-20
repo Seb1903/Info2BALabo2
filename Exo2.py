6a27a25d49cda392cd243b432d32a4426b94347b
Seb1903 <sebastien.martinez99@gmail.com>
2020-02-20 11:29:58 +0100

    Correct problem

--- Exo2.py
+++ Exo2.py
@@ -1,11 +1,25 @@
-# Question 2 explications
+import sys 
+#print('hello', sysargv[1])python Exo2.py Nombres.txt
+with open(sys.argv[1]) as f : 
+    texte = f.readlines()
+    print(texte)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ # Question 2 explications
 # En tapant python file.py dans le terminal, on peut lancer le fichier (et ses fonctions)
 # importer sys 
 # print('hello', sysargv[1]), sysargv représente les arguments de ce que l'on lance dans le terminal(file.py par ex)
-
-import sys 
-#print('hello', sysargv[1])
-with open(str(sys.argv[1]), 'r') as file : 
-    texte = file.read()
-    print(texte)
-    print(sys.argv[1])
